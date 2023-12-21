07bda46abea88ba6c6d218406833b02201fe0dcc
hieudt <hieudt@seatechit.com.vn>
2023-12-21 15:15:05 +0700

    h update send mail

---
+++ app/Mail/MyMail.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class MyMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    private $bill_code;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($param)
+    {
+        $this->bill_code=$param;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Kiểm tra giao dịch mới',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view:'email.noti',
+            with: [
+                'bill_code'=>$this->bill_code
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ resources/views/email/noti.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Email Title</title>
+</head>
+<body>
+<p>Có giao dịch mới, xin hay kiểm tra tài khoản và vào trình quản lý để thao tác.</p>
+<p>Mã giao dịch: {{$bill_code}}</p>
+</body>
+</html>
