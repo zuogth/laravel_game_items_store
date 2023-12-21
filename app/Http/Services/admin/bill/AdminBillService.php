0c347fcd610fa52673d4e42827a6033e37b7eb0f
hods <hods@telsoft.com.vn>
2023-12-21 16:48:10 +0700

    sh bill

--- app/Http/Services/admin/bill/AdminBillService.php
+++ app/Http/Services/admin/bill/AdminBillService.php
@@ -4,6 +4,7 @@ namespace App\Http\Services\admin\bill;
 
 use App\Helpers\Utils;
 use App\Models\Bill;
+use App\Models\Product;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
@@ -31,6 +32,9 @@ class AdminBillService
                     Log::error("BILL STATUS NULL");
                     throw new \Exception();
                 }
+                // Update quantity
+                if (!$this->updateQuantity($bill, $billNew->status))
+                    return response()->json(["STATUS" => 500, "MESSAGES" => 'Sản phẩm trang kho đã hết'], 500);
                 // TODO: VALIDATE
                 $bill->status = $billNew->status;
                 $bill->save();
@@ -41,5 +45,24 @@ class AdminBillService
             return response()->json(["STATUS" => 500, "MESSAGES" => 'Cập trạng thái hoá đơn thất bại '], 500);
         }
 
+    }
+
+    /*
+     * success: sold + quantity
+     * cancel: total_quantity + quantity
+     * */
+
+    private function updateQuantity($bill, $status)
+    {
+        if (Product::where("ID", $bill->product_id)->exists()) {
+            $product = Product::find($bill->product_id);
+            if ($status == 3) {
+                $product->sold = $product->sold + $bill->quantity;
+            } elseif ($status == -1) {
+                $product->total_quantity = $product->total_quantity + $bill->quantity;
+            }
+            $product->save();
+        }
+        return true;
     }
 }
